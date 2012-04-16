989fb5ce80d65476d36ff54a97c61a5d9f68e607
Konstantin Komelin <konstantin.komelin@gmail.com>
2012-04-16 22:58:46 +0400

    Added raw logic for taking a photo.

--- CatchSmile/MainPage.xaml.cs
+++ CatchSmile/MainPage.xaml.cs
@@ -10,15 +10,90 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Microsoft.Devices;
+using System.Windows.Media.Imaging;
+using Microsoft.Xna.Framework.Media;
 
 namespace CatchSmile
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        /// <summary>
+        /// Camera object.
+        /// </summary>
+        private PhotoCamera Camera { get; set; }
+
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Check supported camera types.
+            if (PhotoCamera.IsCameraTypeSupported(CameraType.FrontFacing))
+            {
+                this.Camera = new PhotoCamera(CameraType.FrontFacing);
+            }
+            else if (PhotoCamera.IsCameraTypeSupported(CameraType.Primary))
+            {
+                this.Camera = new PhotoCamera(CameraType.Primary);
+            }
+            else // Display error message if no one type is supported.
+            {
+                MessageBox.Show("Cannot find a camera on this device");
+                return;
+            }
+
+            this.cameraViewBrush.SetSource(this.Camera);
+
+            this.Camera.CaptureImageAvailable += new EventHandler<ContentReadyEventArgs>(Camera_CaptureImageAvailable);
+            this.Camera.CaptureThumbnailAvailable += new EventHandler<ContentReadyEventArgs>(Camera_CaptureThumbnailAvailable);
+        }
+
+        void Camera_CaptureThumbnailAvailable(object sender, ContentReadyEventArgs e)
+        {
+
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+            {
+                BitmapImage image = new BitmapImage();
+
+                image.SetSource(e.ImageStream);
+
+                this.lastShot.Source = image;
+
+                this.lastShotFrame.Visibility = System.Windows.Visibility.Visible;
+
+                this.cameraView.Visibility = System.Windows.Visibility.Collapsed;
+            });
+
+        }
+
+        private void Camera_CaptureImageAvailable(object sender, ContentReadyEventArgs e)
+        {
+
+            Deployment.Current.Dispatcher.BeginInvoke(() =>
+             {
+
+                 MediaLibrary ml = new MediaLibrary();
+
+                 ml.SavePictureToCameraRoll(
+
+                      string.Format("{0:yyyyMMdd-HHmmss}.jpg", DateTime.Now), e.ImageStream);
+
+             });
+
+        }
+
+        /// <summary>
+        /// Process Tap event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listenerTap(object sender, GestureEventArgs e)
+        {
+            this.Camera.CaptureImage();
         }
     }
 }
